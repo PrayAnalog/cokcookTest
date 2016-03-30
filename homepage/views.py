e84ed9e9ca38c0bf6aef4dccf540efaa62091cf3
PrayAnalog <agy0304@gmail.com>
2016-03-30 18:12:38 +0900

    index: name and image

--- homepage/views.py
+++ homepage/views.py
@@ -1,3 +1,16 @@
+# -*- coding: utf-8 -*- 
+
+
 from django.shortcuts import render
 
-# Create your views here.
+from .models import * 
+
+def index(request):
+	food_list_for_sale = Food.objects.filter(state="주문가능")
+	food_list_reserve = Food.objects.filter(state="예약가능")
+	food_list_not_for_sale = Food.objects.filter(state="주문불가능")
+	return render(request, 'homepage/index.html', {'food_list_for_sale':food_list_for_sale, 'food_list_reserve':food_list_reserve, 'food_list_not_for_sale':food_list_not_for_sale})
+
+def detail(request, foodid):
+	food = Food.objects.get(id=foodid)
+	return render(request, 'homepage/detail.html', {'food':food})
